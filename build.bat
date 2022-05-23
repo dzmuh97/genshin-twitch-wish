fa667fcc63f870d18fd27f1bdd7c4a7308a008a4
dzmuh97 <g_smart@mail.ru>
2022-05-23 16:19:26 +0300

    Changed build script

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
 pyinstaller --onefile --icon=icon.ico --name TwitchGenshinWishSim main.py
+del logs\*.*
+copy dist\TwitchGenshinWishSim.exe pack\
+copy config.json pack\
+copy icon.png pack\
+xcopy background\ pack\background\ /E/I
+xcopy fonts\ pack\fonts\ /E/I
+xcopy images\ pack\images\ /E/I
+xcopy sound\ pack\sound\ /E/I
+xcopy logs\ pack\logs\ /E/I
+pause
