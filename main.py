223289806fa83b79f6ce32322e2d3cabd9a0f0cf
dzmuh97 <g_smart@mail.ru>
2022-05-23 17:27:00 +0300

    Push version

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ except jsonschema.ValidationError as _e:
 
 __title__ = 'genshin-twitch-wish'
 __site__ = 'github.com/dzmuh97/genshin-twitch-wish'
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 CONFIG = _config['CONFIG']
 _messages = _config['MESSAGES']
