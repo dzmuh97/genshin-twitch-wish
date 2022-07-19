1ec08b9a9540029cdba0e23d8cdffb1e59251fa8
dzmuh97 <g_smart@mail.ru>
2022-07-19 05:39:16 +0300

    Update build script

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-pyinstaller --noupx --onefile --icon=icon.ico --name TwitchGenshinWishSim main.py
+create-version-file.exe version-metadata.yml --outfile version-metadata.txt
+pyinstaller --clean --noupx --onefile --runtime-tmpdir . --icon=icon.ico --name TwitchGenshinWishSim --version-file version-metadata.txt main.py
 copy dist\TwitchGenshinWishSim.exe pack\
 copy config.json pack\
 copy messages.json pack\
@@ -7,4 +8,4 @@ xcopy background\ pack\background\ /E/I
 xcopy fonts\ pack\fonts\ /E/I
 xcopy images\ pack\images\ /E/I
 xcopy sound\ pack\sound\ /E/I
-pause
+xcopy banners\ pack\banners\ /E/I
