4d5df467d29ee2b2462485a1cbcb107f65914d24
dzmuh97 <g_smart@mail.ru>
2022-07-19 17:52:03 +0300

    Update build script

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 create-version-file.exe version-metadata.yml --outfile version-metadata.txt
 pyinstaller --clean --noupx --onefile --runtime-tmpdir . --icon=icon.ico --name TwitchGenshinWishSim --version-file version-metadata.txt main.py
-copy dist\TwitchGenshinWishSim.exe pack\
-copy config.json pack\
-copy messages.json pack\
-copy icon.png pack\
-xcopy background\ pack\background\ /E/I
-xcopy fonts\ pack\fonts\ /E/I
-xcopy images\ pack\images\ /E/I
-xcopy sound\ pack\sound\ /E/I
-xcopy banners\ pack\banners\ /E/I
+copy dist\TwitchGenshinWishSim.exe genshin-twitch-wish_win64\
+copy config.json genshin-twitch-wish_win64\
+copy messages.json genshin-twitch-wish_win64\
+copy icon.png genshin-twitch-wish_win64\
+xcopy background\ genshin-twitch-wish_win64\background\ /E/I
+xcopy fonts\ genshin-twitch-wish_win64\fonts\ /E/I
+xcopy images\ genshin-twitch-wish_win64\images\ /E/I
+xcopy sound\ genshin-twitch-wish_win64\sound\ /E/I
+xcopy banners\ genshin-twitch-wish_win64\banners\ /E/I
